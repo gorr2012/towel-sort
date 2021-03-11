492409be0ec88b25019f879eb0fa8f7241548695
gorr2012 <sergey_gornost@mail.ru>
2021-03-11 15:23:52 +0300

    refactor: make shorter solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  let res = [];
 
-  if(!matrix || matrix === []) return res;
-  
-  for(let i = 0; i < matrix.length; i++){
-    i % 2 === 1 ?	res = res.concat(matrix[i].reverse()) :	res = res.concat(matrix[i]);
-  }
-  
-  return res;
+  if(!matrix) return [];
+
+  matrix = matrix.map((e,i) => { return i % 2 === 1 ? e.reverse() :	e }).reduce((sum, cur) => { return sum.concat(cur) }, [] );
+
+  return matrix;
 }
